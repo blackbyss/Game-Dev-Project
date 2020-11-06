004293357e590a9f38c96aefa2d305515a3d7a05
Jan Erik Kriisk <j.e.kriisk@gmail.com>
2020-11-06 12:13:23 +0200

    Temp slime char, movement and bounce added

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rigidbody2d;
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody2d.velocity = new Vector2(speed * -1, rigidbody2d.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
+        }
+    }
+}
