ba1fa00a62b582485d56f612a8295787fcd937f0
Jan Erik Kriisk <j.e.kriisk@gmail.com>
2020-11-19 10:10:35 +0200

    Balloon jump idea added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,8 @@ public class Player : MonoBehaviour
 {
     public float speed = 12;
     public int Lives = 3;
-    public float defaultJumpHeight = 25;
+    public float defaultJumpHeight = 35;
+    public int defaultGravity = 8;
     [HideInInspector] public float jumpHeight;
     Rigidbody2D rigidbody2d;
     SpriteRenderer sprite;
@@ -21,6 +22,7 @@ public class Player : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         jumpHeight = defaultJumpHeight;
         spawnPoint = gameObject.transform.position;
+        rigidbody2d.gravityScale = defaultGravity;
     }
 
     string currentBall = "default";
@@ -38,18 +40,27 @@ public class Player : MonoBehaviour
             rigidbody2d.velocity = new Vector2(speed, rigidbody2d.velocity.y);
             sprite.flipX = true;
         }
-        if (Input.GetKey(KeyCode.B))
+        if (Input.GetKey(KeyCode.A))
         {
             currentBall = "doubleBounce";
             jumpHeight = defaultJumpHeight * 0.7f; //Multiplied value is sketchy
         }
-
+        /*
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentBall = "balloon";
+            jumpHeight = defaultJumpHeight * 0.5f;
+            rigidbody2d.gravityScale = 1;
+            transform.localScale = new Vector3(15f, 15f, 1f);
+        }
+        */
         if (Input.GetKey(KeyCode.D))
         {
             currentBall = "default";
             jumpHeight = defaultJumpHeight;
+            rigidbody2d.gravityScale = defaultGravity;
+            transform.localScale = new Vector3(10f, 10f, 1f);
         }
-
         if (currentBall == "doubleBounce")
         {
             if (Input.GetKey(KeyCode.Space) && !doubleJumped)
