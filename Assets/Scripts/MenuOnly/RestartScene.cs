24a31f53be9a631d0c2a75e1de38f162aaa892e5
HannaMariaMagi <hanna.maria.m2gi@gmail.com>
2020-11-22 15:41:43 +0200

    Back to menu fixed

--- Assets/Scripts/MenuOnly/RestartScene.cs
+++ Assets/Scripts/MenuOnly/RestartScene.cs
@@ -7,10 +7,22 @@ using UnityEngine.SceneManagement;
 public class RestartScene : MonoBehaviour
 {
     public Button Button;
+    public AudioSource myFx;
+    public AudioClip HoverSoundFx;
+    public AudioClip ClickSoundFx;
     void Start()
     {
         Button = GetComponent<Button>();
         Button.onClick.AddListener(RestartGame);
+    }
+    private void HoverSound()
+    {
+        myFx.PlayOneShot(HoverSoundFx);
+    }
+
+    private void ClickSound()
+    {
+        myFx.PlayOneShot(ClickSoundFx);
     }
     public void RestartGame()
     {
